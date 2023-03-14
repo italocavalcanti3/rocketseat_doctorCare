475ac1e4ac04be21c632e215f8cbee2d9b5f9f43
Italo Cavalcanti <italobc93@gmail.com>
2023-03-14 14:39:39 -0300

    finaliza mobile

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+window.addEventListener('scroll', onScroll);
+
 const navigation = document.getElementById('navigation');
+const scrollToTopButton = document.querySelector('.scrollToTop');
 
 function onScroll() {
   if (scrollY > 0) {
     navigation.classList.add("scroll");
   } else {
     navigation.classList.remove("scroll");
+  }
+  
+  if (scrollY > 300) {
+    scrollToTopButton.style.opacity = "1";
+  } else {
+    scrollToTopButton.style.opacity = "0";
   }
 }
 
